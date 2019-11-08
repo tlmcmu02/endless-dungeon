584bb674da8a7e7f487d15a2fe7db064589f8110
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-11-08 09:54:08 -0500

    vbfgfg

--- modules/mirrorroom.lua
+++ modules/mirrorroom.lua
@@ -30,6 +30,8 @@ function love.load()
   cooldown = 0
   animtimer = 0
   bhp = 1
+  rx = 464
+  ry = 536
 
   spritesheet = love.graphics.newImage('hero/Old heroT.png')
   grid = anim8.newGrid(64, 64, spritesheet:getWidth(), spritesheet:getHeight())
@@ -51,9 +53,9 @@ function love.load()
   grid = anim8.newGrid(64, 64, spritesheet:getWidth(), spritesheet:getHeight())
   idle1 = anim8.newAnimation(grid('1-4', 1), 0.4)
 
-  rollinspritesheet = love.graphics.newImage('assets-1/enemies/rollin.png')
-  grid = anim8.newGrid(88, 140, 1408, 1172)
-  idle2 = anim8.newAnimation(grid('1-16', 1), 0.1)
+  rollinspritesheet = love.graphics.newImage('assets-1/enemies/rollinA.png')
+  grid = anim8.newGrid(58, 30, 464, 240)
+  idle2 = anim8.newAnimation(grid('1-6', 1), 0.1)
 
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/limestone_6.png')
   cobaltM = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_flip.png')
@@ -333,7 +335,7 @@ function love.update(dt)
   end
 
   if bhp == 1 then
-    if cc(mirrorx, mirrory, w, h, bx, by, w, h) then  
+    if cc(mirrorx + 16, mirrory, w - 24, h, bx, by, w, h) then  
       if Iframes == 0 then
         hpnum = hpnum - 1
         if Iframes == 0 then
@@ -483,7 +485,7 @@ function love.draw()
 if bhp == 1 then
   idle1:draw(bannanaspritesheet, bx, by)
 end
-  idle2:draw(rollinspritesheet, x + 90, y + 90)
+  idle2:draw(rollinspritesheet, rx, ry)
 
 if timerIFrames == 1 or timerIFrames == 2 or timerIFrames == 3 or timerIFrames == 4 or timerIFrames == 5 or timerIFrames == 6 or timerIFrames == 7 or timerIFrames == 8 or timerIFrames == 9 or timerIFrames == 10 or timerIFrames == 11 or timerIFrames == 12 or timerIFrames == 13 or timerIFrames == 14 or timerIFrames == 15 or timerIFrames == 16 or timerIFrames == 17 or timerIFrames == 21 or timerIFrames == 22 or timerIFrames == 23 or timerIFrames == 24 or timerIFrames == 25 or timerIFrames == 26 or timerIFrames == 27 or timerIFrames == 28 or timerIFrames == 29 or timerIFrames == 30 or timerIFrames == 31 or timerIFrames == 32 or timerIFrames == 33 or timerIFrames == 34 or timerIFrames == 35 or timerIFrames == 36 or timerIFrames == 37 or timerIFrames == 40 or timerIFrames == 41 or timerIFrames == 42 or timerIFrames == 43 or timerIFrames == 44 or timerIFrames == 45 or timerIFrames == 46 or timerIFrames == 47 or timerIFrames == 48 or timerIFrames == 49 or timerIFrames == 50 or timerIFrames == 51 or timerIFrames == 52 or timerIFrames == 53 or timerIFrames == 54 or timerIFrames == 55 or timerIFrames == 56 or timerIFrames == 57 or timerIFrames == 0 then
   if anim == idle then
